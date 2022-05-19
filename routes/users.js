a4a35f4283c3a25e98356af821886ce4514d6ab3
aron8112 <aron8112@gmail.com>
2022-05-19 12:16:45 -0300

    OT199-33 Delete User Endpoint

--- routes/users.js
+++ routes/users.js
@@ -38,4 +38,22 @@ async (req, res) => {
 });
 
 
+router.delete('/users/:id', async (req, res) => {
+try {
+  const { id } = req.params;
+  const deleteUser = await User.destroy({ where: {id: id} });
+
+  if(deleteUser){
+    res.status(200).json({ msg: `${deleteUser} was deleted`})
+  } else {
+    res.status(400).json({ msg: 'Cannot delete user'})
+  }
+  
+} catch (error) {
+  res.status(500).json({ msg: 'Something went wrong' });
+}
+
+
+})
+
 module.exports = router;
