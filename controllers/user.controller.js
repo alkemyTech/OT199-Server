65bae3be6e4cefe6f0e4387c51328d82c8f10228
Miguel <chanampamiguel@gmail.com>
2022-05-19 13:55:53 -0300

    Add Exception

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,20 +3,26 @@ const { User } = require('../models');
 
 async function register(req, res) {
     
-    const { firstName, lastName, email, password, photo = null, roleId = null } = req.body;
+    try {
+        const { firstName, lastName, email, password, photo = null, roleId = null } = req.body;
 
-    const user = User.build({ firstName, lastName, email, password, photo, roleId });
+        const user = User.build({ firstName, lastName, email, password, photo, roleId });
 
-    // Encrypt password
-    const salt = bcryptjs.genSaltSync();
-    user.password = bcryptjs.hashSync(password, salt);
+        // Encrypt password
+        const salt = bcryptjs.genSaltSync();
+        user.password = bcryptjs.hashSync(password, salt);
 
-    await user.save();
+        await user.save();
 
-    res.status(200).json({
-        msg: 'Registration has been successful',
-        user
-    });
+        res.status(200).json({
+            msg: 'Registration has been successful',
+            user
+        });
+    } catch (error) {
+        res.status(500).json({
+            msg: error
+        });
+    };
 };
 
 module.exports = {
