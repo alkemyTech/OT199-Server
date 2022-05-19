260b1360434e57447dfde8b95ba10e0651afb27c
Miguel <chanampamiguel@gmail.com>
2022-05-19 13:29:51 -0300

    Create and import register function

---
+++ controllers/user.controller.js
@@ -0,0 +1,10 @@
+const { User } = require('../models');
+
+async function register(req, res) {
+    
+    res.send('Ok!');
+};
+
+module.exports = {
+    register
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { register } = require('../controllers/user.controller');
+
+router.post('/register', register);
 
 module.exports = router;
