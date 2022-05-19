d6cd3593c940f7e208eab1bbec46e80005a0d301
Miguel <chanampamiguel@gmail.com>
2022-05-19 13:33:14 -0300

    Add validations

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator');
 const { register } = require('../controllers/user.controller');
 
-router.post('/register', register);
+router.post('/register', [
+    check('firstName', 'First name is required').not().isEmpty(),
+    check('lastName', 'Last name is required').not().isEmpty(),
+    check('email', 'Email is not valid').isEmail(),
+    check('password', 'Password must contain at least 8 characters, inlcuding uppercase, lowercase and numbers').isStrongPassword({ minSymbols: 0 })
+], register);
 
 module.exports = router;
