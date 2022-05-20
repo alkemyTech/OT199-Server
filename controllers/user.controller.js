84dc3a54aa257e5e17d40d277b8e8725d45abd19
Miguel <chanampamiguel@gmail.com>
2022-05-20 12:50:09 -0300

    Fix trycatch

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,26 +5,26 @@ class UserController {
 
     static async register(req, res) {
         
+        const { firstName, lastName, email, password, image = null, roleId = null } = req.body;
+        
+        const user = User.build({ firstName, lastName, email, password, image, roleId });
+        
+        // Encrypt password
+        const salt = bcryptjs.genSaltSync();
+        user.password = bcryptjs.hashSync(password, salt);
+        
         try {
-            const { firstName, lastName, email, password, image = null, roleId = null } = req.body;
-    
-            const user = User.build({ firstName, lastName, email, password, image, roleId });
-    
-            // Encrypt password
-            const salt = bcryptjs.genSaltSync();
-            user.password = bcryptjs.hashSync(password, salt);
-    
             await user.save();
-    
-            res.status(200).json({
-                msg: 'Registration has been successful',
-                user
-            });
         } catch (error) {
             res.status(500).json({
                 msg: error
             });
         };
+    
+        res.status(200).json({
+            msg: 'Registration has been successful',
+            user
+        });
     };
 };
 
