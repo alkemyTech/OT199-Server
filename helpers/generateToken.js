e2155b314e518eb9500f1b315df8d0a3feed519e
Alexander Ruben Mamani <labm2014@gmail.com>
2022-05-21 10:35:49 -0300

    pasando el codigo generateToken a clases

--- helpers/generateToken.js
+++ helpers/generateToken.js
@@ -1,26 +1,31 @@
 const jwt = require('jsonwebtoken')
 
-// genera el token y con los datos del usuario
-const tokenSign = (user)=>{
-    return jwt.sign(
-        {
-            id:user.id,
-            role:user.roleId
-        },
-        process.env.JWT_SECRET,
-        {
+class generateToken{
+
+    // genera el token y con los datos del usuario
+    static tokenSign(user){
+        return jwt.sign(
+            {
+                id:user.id,
+                role:user.roleId
+            },
+            process.env.JWT_SECRET,
+            {
             expiresIn:"1h"
-        }
-    )
-}
-// verifica que sean los mismos codigos del token
-const verifyToken = async(token)=>{
-    try{
-        return jwt.verify(token,process.env.JWT_SECRET)
-    } catch(e){
-        return null
+            }
+        )
     }
+    // verifica que sean los mismos codigos del token
+    static async verifyToken(token){
+        try{
+            return jwt.verify(token,process.env.JWT_SECRET)
+        } catch(e){
+            return null
+        }
     
+    }
+
 }
 
-module.exports={tokenSign,verifyToken}
+
+module.exports=generateToken
