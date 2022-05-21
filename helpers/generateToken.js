ab7978cc89b44b3040e077c8bd34f4b90728a589
Alexander Ruben Mamani <labm2014@gmail.com>
2022-05-20 21:54:04 -0300

    creando carpeta Helpers y archivo JWT

---
+++ helpers/generateToken.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+
+// genera el token y con los datos del usuario
+const tokenSign = (user)=>{
+    return jwt.sign(
+        {
+            id:user.id,
+            role:user.roleId
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn:"1h"
+        }
+    )
+}
+// verifica que sean los mismos codigos del token
+const verifyToken = async(token)=>{
+    try{
+        return jwt.verify(token,process.env.JWT_SECRET)
+    } catch(e){
+        return null
+    }
+    
+}
+
+module.exports={tokenSign,verifyToken}
