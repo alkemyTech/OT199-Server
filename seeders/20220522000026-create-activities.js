cf551729a2d384bd169d63b5c4dd9026ecbb9941
Miguel <chanampamiguel@gmail.com>
2022-05-22 15:37:27 -0300

    Add rollback logic

--- seeders/20220522000026-create-activities.js
+++ seeders/20220522000026-create-activities.js
@@ -12,11 +12,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Activities', null, {});
   }
 };
