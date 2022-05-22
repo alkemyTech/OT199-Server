1c78f595483211f5a04cd04d5986fc587980e1f3
Miguel <chanampamiguel@gmail.com>
2022-05-21 21:07:29 -0300

    Create Seed Activity

---
+++ seeders/20220522000026-create-activities.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Activities', [{
+      name: 'Activity',
+      content: 'Example text here',
+      image: 'https://drive.google.com/drive/u/2/folders/15S0mymkTX2Tg_dIbAwDmosm-6xc4E95f',
+      createdAt: new Date,
+      updatedAt: new Date
+    }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
