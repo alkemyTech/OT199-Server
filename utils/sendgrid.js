d5695030bf90800b4592716316b5488a1205e3b0
diegocarlosdiaz <diegocarlosdiaz5@gmail.com>
2022-05-23 12:53:27 -0300

    documento archivo sendgrid.js con jsdocs

--- utils/sendgrid.js
+++ utils/sendgrid.js
@@ -1,34 +1,21 @@
-require ('dotenv').config();
-//requerimos dotev.config para configurar nuestras variables de entorno y llamamos a la funcion config
-
+/**
+ * extraemos el paquete sendgrid/mail en la variable sgMail.Con la variable sgMail vamos a tomar la apikey que seteamos en el arcivo .env y
+ * se la damos a sendgrid con la function setApiKey
+ * Esta funcion asincrona sendEmail es la encargada de enviar el email
+ * @param{string} msg Aqui seteamos el email que queremos enviar
+ */
+require('dotenv').config();
 const sgMail = require('sendgrid/mail');
-//extraemos el paquete sendgrid/mail en la variable sgMail
-
 sgMail.setApiKey(process.env.SENDGRID_APIKEY);
-
-//Con la variable sgMail vamos a tomar la apikey que seteamos en el arcivo .env y se la damos a sendgrid con la function setApiKey
-
-  async function sendMail (msg) {
-    try{
+//
+async function sendMail(msg) {
+    try {
         await sgMail.send(msg);
 
-        //Creo una funcion asincrona async await, para enviar el msg, mediante el metodo send que se encuentra en el objeto sgMail
-
-    } catch (error){
+    } catch (error) {
         console.log(error);
-        if (error.response){
+        if (error.response) {
             console.error(error.response.body);
         }
-
-        //Utilizamos catch para ver los errores.
     }
 };
-
-//De la siguiente forma ejecutamos el metodo sendEmail
-
-/* sendMail({
-    to: "req.body.email",
-    from: "abc@gmail.com",
-    subject:"Esta ONG te saluda atentamente!!" ,
-    text: "Lorem impsum..." ,
-}); */
