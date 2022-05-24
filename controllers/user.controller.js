50209794f6bd315015c2a90fbc1000c5da6da48a
aron8112 <aron8112@gmail.com>
2022-05-24 18:47:47 -0300

    resolucion conflicto 3

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -7,31 +7,6 @@ const httpStatus = require('../helpers/httpStatus');
 
 class UserController {
 
-    static async logIn(req, res) {
-        const {
-            body: { email, password },
-        } = req;
-        try {
-            const userFound = await User.findOne({
-                where: { email }
-            });
-
-            if (userFound) {
-                const matchPassword = bcryptjs.compareSync(password, userFound.password);
-                if (matchPassword) {
-                    res.status(httpStatus.OK).send(userFound);
-                } else {
-                    res.status(httpStatus.BAD_REQUEST).json({ msg: 'User or Password Incorrect' });
-                }
-            } else {
-                res.status(httpStatus.BAD_REQUEST).json({ msg: 'User or Password Incorrect' })
-            }
-        } catch (error) {
-            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ msg: 'Something went wrong' });
-        }
-    }
-
-
     static async register(req, res) {
 
         const {
@@ -72,6 +47,31 @@ class UserController {
             user
         });
     };
+
+    static async logIn(req, res) {
+        const {
+            body: { email, password },
+        } = req;
+        try {
+            const userFound = await User.findOne({
+                where: { email }
+            });
+
+            if (userFound) {
+                const matchPassword = bcryptjs.compareSync(password, userFound.password);
+                if (matchPassword) {
+                    res.status(httpStatus.OK).send(userFound);
+                } else {
+                    res.status(httpStatus.BAD_REQUEST).json({ msg: 'User or Password Incorrect' });
+                }
+            } else {
+                res.status(httpStatus.BAD_REQUEST).json({ msg: 'User or Password Incorrect' })
+            }
+        } catch (error) {
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ msg: 'Something went wrong' });
+        }
+    }
+
 };
 
 module.exports = UserController;
