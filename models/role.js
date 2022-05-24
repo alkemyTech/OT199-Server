e367cbe600047dde1304bfb3c8e3c0f554517505
aron8112 <aron8112@gmail.com>
2022-05-24 14:42:12 -0300

    se agrego la primary key en el modelo

--- models/role.js
+++ models/role.js
@@ -15,7 +15,8 @@ module.exports = (sequelize, DataTypes) => {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      autoIncrement: true
+      autoIncrement: true,
+      primaryKey: true
     },
     name: {
       type: DataTypes.STRING,
