a64a89f625d1b5f105799ea031b6fca99b05911f
Alexander Ruben Mamani <labm2014@gmail.com>
2022-05-24 22:27:31 -0300

    arreglando error de rutas de usuarios

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-=======
 var express = require('express');
 var router = express.Router();
 var { check } = require('express-validator');
 var validateFields = require('../helpers/validateFields');
->>>>>>> 2ffecef7042717d7c9381d8e30373b5cb639a140
 const UserController = require('../controllers/user.controller')
 
 /* GET users listing. */
@@ -14,12 +9,9 @@ router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 })
 
-<<<<<<< HEAD
 // Delete user for ID
 router.delete('/users/:id', UserController.deleteUser)
 
-module.exports = router;
-=======
 router.post('/auth/login', [
     check('email', 'Email is not valid').not().isEmpty().isEmail(),
     check('password', 'Password is not valid').not().isEmpty().isString(),
@@ -27,4 +19,3 @@ router.post('/auth/login', [
 ], UserController.logIn)
 
 module.exports = router;
->>>>>>> 2ffecef7042717d7c9381d8e30373b5cb639a140
