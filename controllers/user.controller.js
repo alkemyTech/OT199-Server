f4807c7919ed6f5b7999ca051d4d4fabd266feaf
Gonzalo Parra <gparra@VALKIMIA.COM>
2022-05-26 20:12:11 -0300

    add endpoint update user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -66,7 +66,9 @@ class UserController {
             if (userFound) {
                 const matchPassword = bcryptjs.compareSync(password, userFound.password);
                 if (matchPassword) {
-                    res.status(httpStatus.OK).json({ token: generateToken.tokenSign(userFound) });
+                    res.status(httpStatus.OK).json({
+                        token: generateToken.tokenSign(userFound)
+                    });
 
                 } else {
                     res.status(httpStatus.BAD_REQUEST).json({
@@ -96,6 +98,57 @@ class UserController {
             });
         }
         res.status(httpStatus.OK).send(userList);
+    }
+
+    static async updateDataUser(req, res) {
+
+        const id = req.params.id;
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            image,
+        } = req.body;
+
+        let userFound;
+        try {
+            // recupera en DB por PK
+            userFound = await User.findByPk(id);
+        } catch (error) {
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+                msg: 'Something went wrong'
+            });
+        }
+        if (!userFound) {
+            res.status(httpStatus.NOT_FOUND).json({
+                msg: 'Something went wrong'
+            });
+        } else {
+            // Encrypt password
+            const salt = bcryptjs.genSaltSync();
+            userFound.password = bcryptjs.hashSync(password, salt);
+
+            // update User
+            userFound.firstName = firstName;
+            userFound.lastName = lastName;
+            userFound.email = email;
+            userFound.image = image;
+
+            try {
+                await userFound.save();
+
+                res.status(httpStatus.OK).json({
+                    msg: 'Update has been successful',
+                    userFound
+                });
+            } catch (error) {
+                res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+                    msg: 'Something went wrong'
+                });
+            };
+
+        }
     }
 };
 
