ecafdf0c76fe589ae84b7f0a126ef6a19b3f8dc8
Miguel <chanampamiguel@gmail.com>
2022-05-27 17:42:29 -0300

    Insert news in DB

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -1,8 +1,26 @@
-const News = require('../models/news');
+const httpStatus = require('../helpers/httpStatus');
+const { News } = require('../models');
 
 class NewsController {
 
-}
+    static async createNews(req, res) {
+        
+        let news = {};
+        const { name, image, content, categoryId } = req.body;
+
+        try {
+            news = await News.create({ name, image, content, categoryId });
+        } catch (error) {
+            return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+                msg: error
+            });
+        };
+
+        res.status(httpStatus.OK).json({
+            msg: '',
+            data: news
+        });
+    };
+};
 
 module.exports = NewsController;
-
