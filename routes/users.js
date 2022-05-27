98f52fbfde890fa6ce66252fc0fe047f99a73fff
Miguel <chanampamiguel@gmail.com>
2022-05-27 17:39:36 -0300

    Fix import error

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { check } = require('express-validator');
-const validateFields = require('../helpers/validateFields');
 const UserController = require('../controllers/user.controller');
 const CheckRoleId = require('../middlewares/checkRole');
 
 /* GET users listing. */
 router.get('/', CheckRoleId.isAdmin, UserController.getAll);
-
-
 
 module.exports = router;
