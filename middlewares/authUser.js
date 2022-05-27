e97d99deafb17604b9e712af233324575ac96457
diegocarlosdiaz <diegocarlosdiaz5@gmail.com>
2022-05-27 18:12:43 -0300

    Agrego funcionalidad al middleware de token

--- middlewares/authUser.js
+++ middlewares/authUser.js
@@ -4,20 +4,37 @@
  *  
  */
 
-const token= require("../helpers/generateToken")
-async function AuthUser (req, res, next) {
-   
-   console.log(req.headers);
-    if (!req.headers.authorization) {
-        res.status(403).json({ msg: "Acceso no autorizado" });
-    } else {
+const { verifyToken } = require("../helpers/generateToken");
 
-       /*  let tokenVerificado = await token.verifyToken(req.headers.authorization)
-        console.log(tokenVerificado); */
+async function AuthUser(req, res, next) {
+
+    try {
+
+        if (!req.headers.authorization) {
+            res.status(403).json({ msg: "Acceso no autorizado" });
+            return;
+
+        }
+
+        const token = req.headers.authorization.split(" ").pop();
+
+        const tokenData = await verifyToken(token);
         
-        next();
+        if (tokenData.id){
+            next();
+        }
+        else{
+            res.status(403).json({ msg: "Acceso no autorizado" });
+        }
+
+
+    } 
+        catch (error) {
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+                msg: 'Something went wrong'
+            });
 
     }
 }
 
-module.exports = {AuthUser};
+module.exports = { AuthUser };
