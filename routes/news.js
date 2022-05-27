58d326439b8c8a0951d7cdeed85cb365fc4520bc
Miguel <chanampamiguel@gmail.com>
2022-05-27 17:44:57 -0300

    Add validations

--- routes/news.js
+++ routes/news.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator');
 const NewsController = require('../controllers/newsController');
+const Validator = require('../helpers/validator');
 
-router.post('/', NewsController.createNews);
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('image', 'Image is required').not().isEmpty(),
+    check('content', 'Content is required').not().isEmpty(),
+    check('categoryId', 'CategoryId is required').not().isEmpty(),
+    Validator.validateFields
+], NewsController.createNews);
 
 module.exports = router;
