cdd05969c93dac2142794a73e463756349f5e7b8
Exequiel65 <marcos97britos@gmail.com>
2022-05-28 13:47:06 -0300

    add method

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,25 +1,42 @@
-const {Categorie} = require('../models')
-
-class Categories{
-  static async getAllCategories( req, res ){
+const { Categorie } = require('../models')
+const httpStatus = require('../helpers/httpStatus')
+class Categories {
+  static async getAllCategories(req, res) {
     try {
       let resolve = await Categorie.findAll();
-      res.status(200).json({
-        meta : {
-          resposne : true
-        },
-        data : resolve
+      res.status(httpStatus.OK).json({
+        data: resolve
       });
     } catch (error) {
-      res.status(500).json({
-        meta : {
-          resposne : false
-        },
+      res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
         error
       });
     }
   };
-  
+
+  static async updateCategories(req, res) {
+    let idParams = req.params.id
+    try {
+      let response = await Categorie.findByPk(idParams)
+      if (response) {
+        let resolve = await Categorie.update({ ...req.body }, { where: { id: idParams } });
+        res.status(httpStatus.OK).json({
+          msg: "Successful registry update"
+        })
+
+      }
+      res.status(httpStatus.NOT_FOUND).json({
+        msg: "A record with the set parameter was not found"
+      })
+
+
+    } catch (error) {
+      res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+        error
+      })
+    }
+  }
+
 };
 
-module.exports =  Categories
+module.exports = Categories
