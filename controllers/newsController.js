fc09d3e3b3e67fe880c33273aad3803447a136bb
ttorr3 <torretomas98@gmail.com>
2022-05-29 12:57:58 -0300

    Endpoint for news detail

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -1,8 +1,38 @@
-const News = require('../models/news');
+const { News } = require('../models');
+const httpStatus = require('../helpers/httpStatus');
 
 class NewsController {
+  static async getDetail(req, res){
+    const { id } = req.params;
+    let detail = null;
 
+    try {
+      detail = await News.findByPk(id);
+    } catch (error){
+      res  
+        .status(httpStatus.INTERNAL_SERVER_ERROR)
+        .send(error);
+      return;
+    }
+
+    if(!detail){
+      res
+        .status(httpStatus.NOT_FOUND)
+        .send("New not found");
+      return;
+    }
+    
+    if(!detail){
+      res
+        .status(httpStatus.NOT_FOUND)
+        .send("New not found");
+      return;
+    }
+
+    res
+      .status(httpStatus.OK)
+      .send(detail);
+  }
 }
 
 module.exports = NewsController;
-
