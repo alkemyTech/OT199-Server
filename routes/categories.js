e5f150393847b877a5dedf0b4f6d8dcb958683b7
diegocarlosdiaz <diegocarlosdiaz5@gmail.com>
2022-05-30 02:10:21 -0300

    borro el requerimiento del middleware authUser ya que no se esta utilizando en ese momento

--- routes/categories.js
+++ routes/categories.js
@@ -1,8 +1,8 @@
 let express = require('express');
 const Categories = require('../controllers/categoriesController');
 const CheckRoleId = require('../middlewares/checkRole');
-let router = express.Router();
-let {AuthUser} = require("../middlewares/authUser")
+const router = express.Router();
+
 
 router.get('/', CheckRoleId.isAdmin, Categories.getAllCategories);
 
