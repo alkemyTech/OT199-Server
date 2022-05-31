d9894f58b67155a6fa4f64114f19e3a7a54f2939
Alexander Ruben Mamani <labm2014@gmail.com>
2022-05-30 23:13:44 -0300

    fix merge

--- migrations/20220518132904-create-organization.js
+++ migrations/20220518132904-create-organization.js
@@ -7,7 +7,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         autoIncrement: true,
-        primaryKey: true,       
+        primaryKey: true,
       },
       name: {
         type: Sequelize.STRING,
