a2552fb944237f798c367a5be25d7e2504bc876f
Gonzalo Parra <gparra@VALKIMIA.COM>
2022-05-31 12:36:37 -0300

    fix merge development

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,9 @@ const CheckRoleId = require('../middlewares/checkRole');
 /* GET users listing. */
 router.get('/', CheckRoleId.isAdmin, UserController.getAll);
 
+
+// Delete user for ID
+router.delete('/:id',CheckRoleId.isUserLoggedIn ,UserController.deleteUser)
 /**
  * PATCH Update user
  * @param {number} id - The id of user
@@ -29,17 +32,5 @@ router.patch('/:id', CheckRoleId.isUserLoggedIn, [
     }), Validator.validateFields
 ], UserController.updateDataUser);
 
-/**
- * PATCH Update user
- * @param {number} id - The id of user
- */
-router.patch('/:id', [
-    check('firstName', 'First name is required').not().isEmpty(),
-    check('lastName', 'Last name is required').not().isEmpty(),
-    check('email', 'Email is not valid').isEmail(),
-    check('password', 'Password must contain at least 8 characters, inlcuding uppercase, lowercase and numbers').isStrongPassword({
-        minSymbols: 0
-    }), Validator.validateFields
-], UserController.updateDataUser);
 
 module.exports = router;
