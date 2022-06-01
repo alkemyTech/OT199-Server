56825043cc40781cf2433d0303cb09be1e417f29
aron8112 <aron8112@gmail.com>
2022-06-01 14:40:01 -0300

    Commit con correcciones

--- constants/rolesUser.js
+++ constants/rolesUser.js
@@ -1,21 +1,16 @@
 class RolesUser {
   static Roles = {
     Admin: 1,
-    Estudiante: 2,
-    Docente: 3,
-    Voluntario: 4,
-    Tutor: 5,
-    Colaborador: 6,
-    Visitante: 7
+    Standard: 2
   }
 
-  static RolesPermissions_GET = [1, 2, 3, 4, 5, 6, 7]
+  static RolesPermissions_GET = [1, 2]
 
-  static RolesPermissions_POST = [1, 3, 5]
+  static RolesPermissions_POST = [1]
 
-  static RolesPermissions_PUT = [1, 3]
+  static RolesPermissions_PUT = [1]
 
-  static RolesPermissions_PATCH = [1, 2, 3]
+  static RolesPermissions_PATCH = [1, 2]
 
   static RolesPermissions_DELETE = [1]
 }
--- controllers/userController.js
+++ controllers/userController.js
@@ -8,20 +8,20 @@ class UserController {
 
     static async deleteUser(req, res) {
         try {
-          const { id } = req.params;
-          const deleteUser = await User.destroy({ where: { id: +id } });
-          if (deleteUser) {
-            return res.status(httpStatus.OK).send({ msg: `the User was deleted` });
-          }
-          return res
-            .status(httpStatus.BAD_REQUEST)
-            .json({ msg: "Cannot delete user" });
+            const { id } = req.params;
+            const deleteUser = await User.destroy({ where: { id: +id } });
+            if (deleteUser) {
+                return res.status(httpStatus.OK).send({ msg: `the User was deleted` });
+            }
+            return res
+                .status(httpStatus.BAD_REQUEST)
+                .json({ msg: "Cannot delete user" });
         } catch (error) {
-          res
-            .status(httpStatus.INTERNAL_SERVER_ERROR)
-            .json({ msg: "Something went wrong" });
+            res
+                .status(httpStatus.INTERNAL_SERVER_ERROR)
+                .json({ msg: "Something went wrong" });
         }
-      }
+    }
 
     static async register(req, res) {
 
@@ -45,7 +45,7 @@ class UserController {
 
         // Encrypt password
         const salt = bcryptjs.genSaltSync(10);
-        const hashPass = bcryptjs.hashSync(user.password, salt);
+        bcryptjs.hashSync(user.password, salt);
 
         //Access_token
         const token = generateToken.tokenSign(user);
