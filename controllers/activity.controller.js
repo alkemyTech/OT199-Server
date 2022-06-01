fb71081099bcd104c16cbd54bde401c8eedef614
aron8112 <aron8112@gmail.com>
2022-05-31 23:22:01 -0300

    Conflicting files resolved

--- controllers/activity.controller.js
+++ controllers/activity.controller.js
@@ -1,22 +1,53 @@
-<<<<<<< HEAD
 const httpStatus = require("../helpers/httpStatus");
 const { Activity } = require("../models");
 
 class ActivityController {
+
     static async getActivities(req, res) {
         try {
             const getAllAct = await Activity.findAll();
-=======
-const { Activity } = require('../models');
-const httpStatus = require('../helpers/httpStatus');
+            res.status(httpStatus.OK).json(getAllAct);
+        } catch (error) {
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ msg: error.array });
+        }
+    }
 
-class ActivityController {
+    static async getOneActivity(req, res) {
+        const { name } = req.query;
+
+        try {
+            const getOneAct = await Activity.findOne({
+                where: { name },
+                attributes: ['name', 'content', 'image', 'deletedAt'],
+            });
+
+            res.status(httpStatus.OK).json(
+                getOneAct);
+        } catch (error) {
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ msg: error.array });
+        }
+    }
+
+
+    static async deleteActivities(req, res) {
+        const { name } = req.query;
+
+        try {
+            const delAct = await Activity.destroy({ where: { name } });
+
+            res.status(httpStatus.OK).json({ msg: `The ${delAct.name} was deleted` });
+
+        } catch (error) {
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ msg: error.array });
+        }
+    }
+
     static async updateActivity(req, res) {
-        
+
         let activity = {};
         const { id } = req.params;
         const { name, content, image } = req.body;
-        
+
         try {
             activity = await Activity.findByPk(id);
         } catch (error) {
@@ -49,46 +80,6 @@ class ActivityController {
         });
     };
 };
->>>>>>> origin/development
 
-            res
-                .status(httpStatus.OK)
-                .json(
-                    getAllAct
-                );
-        } catch (error) {
-            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ msg: error.array });
-        }
-    }
-
-    static async getOneActivity(req, res) {
-        const { name } = req.query;
-
-        try {
-            const getOneAct = await Activity.findOne({
-                where: { name },
-                attributes: ['name', 'content', 'image', 'deletedAt'],
-            });
-
-            res.status(httpStatus.OK).json(
-                getOneAct);
-        } catch (error) {
-            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ msg: error.array });
-        }
-    }
-
-    static async deleteActivities(req, res) {
-        const { name } = req.query;
-
-        try {
-            const delAct = await Activity.destroy({ where: { name } });
-
-            res.status(httpStatus.OK).json({ msg: `The ${delAct.name} was deleted` });
-
-        } catch (error) {
-            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ msg: error.array });
-        }
-    }
-}
 
 module.exports = ActivityController;
