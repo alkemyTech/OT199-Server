9c483e91db6d7c152a6ce196cf77970c8cb7f7a9
Miguel <chanampamiguel@gmail.com>
2022-06-01 23:35:38 -0300

    Add enpoint DELETE /members/:id

--- routes/members.js
+++ routes/members.js
@@ -2,5 +2,6 @@ const express = require('express');
 const MemberController = require('../controllers/memberController');
 const router = express.Router();
 
+router.delete('/:id', MemberController.deleteMember);
 
 module.exports = router;
