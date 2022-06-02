395dbaaa9d52c8b57ae3119200c7b1048d2aadab
Miguel <chanampamiguel@gmail.com>
2022-06-02 18:48:45 -0300

    Create deleteTestimonial function

--- controllers/testimonialsController.js
+++ controllers/testimonialsController.js
@@ -1,7 +1,32 @@
-const Testimonial = require('../models/testimonials');
+const { Testimonials } = require('../models');
+const httpStatus = require('../helpers/httpStatus');
+const httpResponses = require('../constants/httpResponses');
 
 class TestimonialController {
 
+  static async deleteTestimonial(req, res) {
+
+    let testimonialDeleted = {};
+    const { id } = req.params;
+
+    try {
+      testimonialDeleted = await Testimonials.destroy({ where: { id }});
+    } catch (error) {
+      res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+        msg: httpResponses.RESPONSE_INTERNAL_SERVER_ERROR
+      });
+    };
+
+    if (!testimonialDeleted) {
+      return res.status(httpStatus.NOT_FOUND).json({
+        msg: 'Testimonial does not exist'
+      });
+    };
+
+    res.status(httpStatus.OK).json({
+      msg: 'Testimonial was deleted successfully',
+    });
+  };
 } 
 
 module.exports = TestimonialController;
