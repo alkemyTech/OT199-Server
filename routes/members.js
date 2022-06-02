7c5bf643eecc0fece18359d46aca578b7112ec44
Miguel <chanampamiguel@gmail.com>
2022-06-02 19:29:28 -0300

    Add user role validation

--- routes/members.js
+++ routes/members.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const MemberController = require('../controllers/memberController');
+const CheckRoleId = require('../middlewares/checkRole');
 const router = express.Router();
 
-router.delete('/:id', MemberController.deleteMember);
+router.delete('/:id',CheckRoleId.isAdmin, MemberController.deleteMember);
 
 module.exports = router;
