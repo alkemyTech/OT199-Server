87d393fbdc8c26932676ec3bb78ea6b2c62a58a2
aron8112 <aron8112@gmail.com>
2022-06-02 11:16:08 -0300

    middleware isUserLoggedIn en POST

--- routes/members.js
+++ routes/members.js
@@ -3,12 +3,13 @@ const MemberController = require('../controllers/memberController');
 const router = express.Router();
 const { check } = require('express-validator');
 const Validator = require('../helpers/validator');
+const CheckRole = require('../middlewares/checkRole');
 
 router.delete('/:id', MemberController.deleteMember);
 router.post('/', [
     check('name', 'Must have a full name').notEmpty().isString(),
     check('email', 'Must have a valid email').notEmpty().isEmail(),
     Validator.validateFields
-], MemberController.createMember)
+], CheckRole.isUserLoggedIn, MemberController.createMember)
 
 module.exports = router;
