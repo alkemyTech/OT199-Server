482a05cf25cb06aeea7373b417d1b6acccc0f7a0
Alexander Ruben Mamani <labm2014@gmail.com>
2022-06-05 19:34:33 -0300

    arreglando la posicion del try en contactController

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,12 +3,10 @@ const httpStatus = require("../helpers/httpStatus");
 const httpResponses = require("../constants/httpResponses");
 
 class ContactController {
-  static async getAllContacts(req, res) {
-
-    const getAllContact = await Contact.findAll();
-    res.status(httpStatus.OK).json(getAllContact);
-    
+  static async getAllContacts(req, res) {   
     try {
+      const getAllContact = await Contact.findAll();
+      res.status(httpStatus.OK).json(getAllContact);
     } catch (error) {
       res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
         msg: httpResponses.RESPONSE_INTERNAL_SERVER_ERROR,
