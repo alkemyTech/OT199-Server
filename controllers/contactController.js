f36befa1329e12c4e7bdd82989090ae84ba851e4
Alexander Ruben Mamani <labm2014@gmail.com>
2022-06-06 20:57:33 -0300

    cambiando el return fuera del try

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,15 +3,17 @@ const httpStatus = require("../helpers/httpStatus");
 const httpResponses = require("../constants/httpResponses");
 
 class ContactController {
-  static async getAllContacts(req, res) {   
+  static async getAllContacts(req, res) {  
+    let getAllContact=[]; 
     try {
-      const getAllContact = await Contact.findAll({ attributes: ['name', 'phone', 'email', 'message', 'createdAt'] });
-      res.status(httpStatus.OK).json(getAllContact);
+      getAllContact = await Contact.findAll({ attributes: ['name', 'phone', 'email', 'message', 'createdAt'] });
+      
     } catch (error) {
-      res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
         msg: httpResponses.RESPONSE_INTERNAL_SERVER_ERROR,
       });
     }
+    return res.status(httpStatus.OK).json(getAllContact);
   }
 }
 
