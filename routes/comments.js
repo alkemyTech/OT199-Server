72b946e5680accab472ddf3f05d0afb1879cdf18
ttorr3 <torretomas98@gmail.com>
2022-06-13 20:23:00 -0300

    validation for adming o comment owner added

--- routes/comments.js
+++ routes/comments.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const CommentsController = require('../controllers/commentsController');
 const Validator = require('../helpers/validator');
 const { check } = require('express-validator');
+const checkRole = require('../middlewares/checkRole');
 
 router.put('/:id',[
   check('body', 'Body is required').notEmpty(),
+  checkRole.isOwnerComment,
   Validator.validateFields,
 ], CommentsController.update);
 
