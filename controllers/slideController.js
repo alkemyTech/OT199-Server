1ac5c5b5070b3ce0f02f709971edc80d6fe138d2
Alexander Ruben Mamani <labm2014@gmail.com>
2022-06-12 21:27:49 -0300

    agregando helper para decoding image y terminando controlador para crear slide

--- controllers/slideController.js
+++ controllers/slideController.js
@@ -1,7 +1,9 @@
-const { Slide } = require('../models');
-const httpStatus = require('../helpers/httpStatus');
-const httpResponses = require('../constants/httpResponses');
-const QueryHelper = require('../helpers/queryHelper');
+const { Slide } = require("../models");
+const httpStatus = require("../helpers/httpStatus");
+const httpResponses = require("../constants/httpResponses");
+const QueryHelper = require("../helpers/queryHelper");
+const DecodeImage = require("../helpers/decodeImage");
+const Uploader = require("../services/uploader");
 
 class SlideController {
   static async getDetail(req, res) {
@@ -9,118 +11,129 @@ class SlideController {
     let detail = undefined;
     try {
       detail = await Slide.findByPk(idParam);
-    }
-    catch (error) {
+    } catch (error) {
       res.status(httpStatus.INTERNAL_SERVER_ERROR);
       res.send({
-        msg: httpResponses.RESPONSE_INTERNAL_SERVER_ERROR
+        msg: httpResponses.RESPONSE_INTERNAL_SERVER_ERROR,
       });
       return;
     }
     if (!detail) {
-      res
-        .status(httpStatus.NOT_FOUND)
-        .send({
-          msg: 'Slide not found'
-        });
+      res.status(httpStatus.NOT_FOUND).send({
+        msg: "Slide not found",
+      });
       return;
     }
-    res
-      .status(httpStatus.OK)
-      .send({
-        msg: 'Slide found succesfully',
-        detail
-      });
+    res.status(httpStatus.OK).send({
+      msg: "Slide found succesfully",
+      detail,
+    });
   }
   static async getAll(req, res) {
-
     let SlideList;
     try {
       SlideList = await Slide.findAll({
-        attributes: ['imageUrl', 'text', 'order']
+        attributes: ["imageUrl", "text", "order"],
       });
     } catch (error) {
-      return res
-        .status(httpStatus.INTERNAL_SERVER_ERROR)
-        .json({
-          msg: httpResponses.RESPONSE_INTERNAL_SERVER_ERROR
-        });
+      return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+        msg: httpResponses.RESPONSE_INTERNAL_SERVER_ERROR,
+      });
     }
-    return res
-      .status(httpStatus.OK)
-      .json(SlideList);
+    return res.status(httpStatus.OK).json(SlideList);
   }
 
   static async delete(req, res) {
-    const { id } = req.params
+    const { id } = req.params;
     let destroyedSlide = undefined;
 
     try {
       destroyedSlide = await Slide.destroy({ where: { id } });
     } catch (error) {
       res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-        msg: httpResponses.INTERNAL_SERVER_ERROR
+        msg: httpResponses.INTERNAL_SERVER_ERROR,
       });
     }
 
     if (!destroyedSlide) {
       return res.status(httpStatus.NOT_FOUND).json({
-        msg: 'Slide not found'
+        msg: "Slide not found",
       });
     }
 
     res.status(httpStatus.OK).json({
-      msg: 'Succesfully slide delete'
+      msg: "Succesfully slide delete",
     });
   }
 
-  static async update(req,res){
-
-    const allowedParameters = ['imageUrl','text','organizationId','order'];
+  static async update(req, res) {
+    const allowedParameters = ["imageUrl", "text", "organizationId", "order"];
     const { id } = req.params;
     const query = QueryHelper.filterBody(allowedParameters, req.body);
 
     let slide;
     try {
-      slide = await Slide.update(query,{where:{id}});
+      slide = await Slide.update(query, { where: { id } });
     } catch (error) {
-        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-            msg: httpResponses.RESPONSE_INTERNAL_SERVER_ERROR
-        });
-    };
+      return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+        msg: httpResponses.RESPONSE_INTERNAL_SERVER_ERROR,
+      });
+    }
 
     if (!slide[0]) {
-        return res.status(httpStatus.NOT_FOUND).json({
-            msg: 'Slide does not exist'
-        });
-    };
+      return res.status(httpStatus.NOT_FOUND).json({
+        msg: "Slide does not exist",
+      });
+    }
 
     res.status(httpStatus.OK).json({
-        msg: 'Slide was updated successfully',
+      msg: "Slide was updated successfully",
     });
-  };
+  }
 
   static async createSlide(req, res) {
+    const { imageUrl, text, order, organizationId } = req.body;
+    let slide;
 
-    const {
-        imageUrl,
-        text,
-        order,
-        organizationId
-    } = req.body;
-    
     try {
-        return res.json(req.body)
+      const data = await DecodeImage.getImageDecoded(imageUrl);
+      if (data === null) {
+        return res.status(httpStatus.NOT_MODIFIED).json({
+          msg: "Not Modified",
+        });
+      }
+
+      const urlLocation = await Uploader.imgUploadAWS(data,text)
+      
+      if (order === " ") {
+        const lastOrder = await Slide.findOne({
+          order: [["order", "DESC"]],
+          attributes: ["order"],
+        });
+        const { order } = lastOrder;
+        slide = Slide.build({...req.body,imageUrl:urlLocation,order:order+1});
+  
+        await slide.save();
         
+      }
+
+      if(typeof(order) === "number"){
+        slide = Slide.build({...req.body,imageUrl:urlLocation});
+  
+        await slide.save();
+      }
+      
+      
     } catch (error) {
-        return res
-            .status(httpStatus.INTERNAL_SERVER_ERROR)
-            .json({
-                msg: httpResponses.RESPONSE_INTERNAL_SERVER_ERROR
-            });
+      return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+        msg: httpResponses.RESPONSE_INTERNAL_SERVER_ERROR,
+      });
     }
-    
-};
-};
+
+    res.status(httpStatus.OK).json({
+      msg: "Slide has been successful",
+    });
+  }
+}
 
 module.exports = SlideController;
