32c635987df11adcccfa3d546c1c7cc77ede9ae6
ttorr3 <torretomas98@gmail.com>
2022-06-13 15:27:45 -0300

    Improve performance

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,24 +1,22 @@
 const { Comment } = require('../models');
 const httpStatus = require('../helpers/httpStatus');
 const httpResponses = require('../constants/httpResponses');
-const QueryHelper = require('../helpers/queryHelper');
 
 class CommentsController{
   static async update(req,res){
     const { id } = req.params;
-    const allowedParameters = ['body'];
-    const query = QueryHelper.filterBody(allowedParameters, req.body);
+    const { body } = req.body;
 
     let comment;
     try {
-      comment = await Comment.update(query,{where:{id}});
+     comment = await Comment.update(body,{where:{id}});
     } catch (error) {
         return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
             msg: httpResponses.RESPONSE_INTERNAL_SERVER_ERROR
         });
     };
 
-    if (!commnet[0]) {
+    if (!comment[0]) {
         return res.status(httpStatus.NOT_FOUND).json({
             msg: 'Comment does not exist'
         });
